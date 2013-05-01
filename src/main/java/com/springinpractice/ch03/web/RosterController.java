3dbe482f9c0c6b8e715bb2cfaf2de72b479281a3
Willie Wheeler <willie.wheeler@gmail.com>
2013-05-01 02:04:09 -0700

    Cleanup

--- src/main/java/com/springinpractice/ch03/web/RosterController.java
+++ src/main/java/com/springinpractice/ch03/web/RosterController.java
@@ -1,4 +1,10 @@
-// Source project: sip03, branch: 01 (Maven Project)
+/* 
+ * Copyright (c) 2013 Manning Publications Co.
+ * 
+ * Book: http://manning.com/wheeler/
+ * Blog: http://springinpractice.com/
+ * Code: https://github.com/springinpractice
+ */
 package com.springinpractice.ch03.web;
 
 import java.util.ArrayList;
@@ -10,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.springinpractice.ch03.model.Member;
-
 
 @Controller
 public final class RosterController {
